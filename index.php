d918f45994cbfc05c48a8d4a1d95434a50ca71a7
GsusWeb <65841502+GsusWeb@users.noreply.github.com>
2020-06-15 21:56:16 +0900

    Yech.pg #1 Basic Test

---
+++ index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="main.css">
+    <title>예찬교회</title>
+  </head>
+
+  <body>
+
+<div class="basicbackground">
+
+
+
+
+
+  <div class="header_Pack">
+    <div class="title">
+      <a href="index.php">
+        <div class="title_description">
+          <p id="title_description">
+          예수님으로 가득 찬
+          </p>
+          </div>
+          <h2>
+        <div class="title_name">
+        예찬교회
+      </div>
+      </h2>
+      </a>
+    </div>
+    <div id="nav">
+       <ul class="nav_list">
+      <span><a href="#">소개</a></span>
+      <span><a href="#">예배</a></span>
+      <span><a href="#">말씀</a></span>
+      <span><a href="#">강독</a></span>
+      <span><a href="#">섬김</a></span>
+      <span><a href="#">연락</a></span>
+
+       </ul>
+       </div>
+  </div>
+
+
+
+    <!-- <div class="first_border">
+    <hr>
+    </div> -->
+
+
+
+<br><br><br><br>
+  <p>
+    <div class="slogan">
+      <div class="outer">
+      <div class="bg">
+        <div class="inner">
+          <h2>
+          <a href="index.php?id=slogan">
+        말씀으로 제자되고 선교하여 제자삼자
+        <div class="logos_chapter">
+          마태복음 28:18-20
+        </div>
+          </a>
+          </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </p>
+
+<div class="second_border">
+<hr>
+</div>
+
+  <p>
+     <div id="worship_title_ONE">
+      <div class="outer">
+      <div class="bg">
+        <div class="inner">
+          <h2>
+          <a href="index.php?id=time_table">
+          예배 시간 안내
+          </a>
+          </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </p>
+
+
+
+<hr>
+
+<div class="youtube_intro">
+  <p>
+    <div class="youtube_description">
+      <div class="outer">
+      <div class="bg">
+        <div class="inner">
+          <h2>
+          <a href="https://www.youtube.com/channel/UCpk8N5i2UV1d9qvMNBVpfvQ/playlists" target="_blank">
+          알파와 오메가 성경강해시리즈
+          </h2>
+          </div>
+        </div>
+        <br>
+        <div class="You">You</div>
+          <div class="tube">Tube</div>
+      </div>
+    </a>
+  </div>
+
+</div>
+</p>
+
+<hr>
+
+<p>
+  <div class="book_intro">
+    <div class="outer">
+    <div class="bg">
+      <div class="inner">
+        <h2>
+        <a href="index.php?id=book_intro">
+        하나님의 말씀으로 마음에 꽃이 피다
+        </a>
+        </h2>
+        </div>
+      </div>
+    </div>
+   </div>
+</p>
+
+<hr>
+
+<p>
+  <div class="support_mission">
+    <div class="outer">
+    <div class="bg">
+      <div class="inner">
+        <h2>
+        <a href="index.php?id=mission">
+        섬기는 교역지
+        </h2>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</p>
+<hr>
+
+
+
+
+
+
+
+
+
+
+    </div>
+  </body>
+</html>
